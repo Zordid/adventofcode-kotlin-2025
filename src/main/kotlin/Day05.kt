0a6f50cf3c3e78e66759c7f687ecd1e497b3fa43
Olaf <4798822+Zordid@users.noreply.github.com>
2025-12-05 07:09:14 +0100

    Refactor day 5 solution to simplify and optimize range merging logic

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -7,43 +7,24 @@ class Day05 : Day(5, 2025, "Cafeteria") {
 
     val ingredients = input.sections[1].lines.map { it.toLong() }
 
-    override fun part1(): Any? {
-        return ingredients.count { i ->
-            val isFresh = freshIngredientRanges.any { r -> i in r }
-            log { "$i is fresh: $isFresh" }
-            isFresh
-        }
+    override fun part1() = ingredients.count { i ->
+        freshIngredientRanges.any { r -> i in r }
     }
 
-    override fun part2(): Any? {
-        val ranges = freshIngredientRanges.sortedBy { it.first }.toMutableList()
-        var merged: Boolean
-        do {
-            ranges.withIndex().zipWithNext().forEach { (r1i, r2i) ->
-                val (idx, r1) = r1i
-                val (_, r2) = r2i
+    override fun part2(): Long {
+        val mergedRanges = freshIngredientRanges.sortedBy { it.first }.fold(emptyList<LongRange>()) { acc, next ->
+            val prev = acc.lastOrNull() ?: return@fold listOf(next)
 
-                // complete within
-                if (r2.first in r1 && r2.last in r1) {
-                    ranges[idx + 1] = 0L..-1
-                }
-                // direct attach
-                else if (r2.first == r1.last + 1) {
-                    ranges[idx] = r1.first..r2.last
-                    ranges[idx + 1] = 0L..-1
-                } else if (r2.first in r1 && r2.last !in r1) {
-                    ranges[idx] = r1.first..r2.last
-                    ranges[idx + 1] = 0L..-1
-                }
+            when {
+                // (-----) [~~~~~]  not touching at all
+                next.first > prev.last + 1 -> acc.plusElement(next)
+                // (----[===)~~~~]  overlapping or touching
+                next.last > prev.last -> acc.dropLast(1).plusElement(prev.first..next.last)
+                // (----[===]----)  completely overlapping
+                else -> acc
             }
-            val before = ranges.size
-            ranges.removeIf { it.size == 0L }
-            merged = ranges.size != before
-        } while (merged)
-
-        log { ranges.joinToString("\n") }
-
-        return ranges.sumOf { it.size }
+        }
+        return mergedRanges.sumOf { it.size }
     }
 
 }
